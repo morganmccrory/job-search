59bbd6d5d6aec36b58fe5d44bfe2165990f056a3
morganmccrory <morganmccrory@gmail.com>
2016-09-30 01:00:10 -0500

    Add seeded data to search params

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,13 @@
 class SearchController < ApplicationController
   def index
+    @locations = []
+    Location.all.each do |location|
+      @locations << "#{location.city}, #{location.state}"
+    end
+    @levels = Level.all
+    @companies = Company.all
+    @categories = Category.all
+
     @search = Search.new(0, params[:company], params[:location], params[:level], params[:category])
     # @total_pages_available = @response["page_count"]
   end
